837d22306c0e1bad7c645a356d199eabd13fa7cb
ZYMON <2243856@slu.edu.ph>
2026-05-11 02:44:36 +0800

    FRONTEND FIXES

--- frontend/src/components/AppointmentCard.js
+++ frontend/src/components/AppointmentCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+
 export default function AppointmentCard({ date, vetName, service }) {
     return (
         <View style={styles.card}>
