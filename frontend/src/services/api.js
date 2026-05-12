b1aa5d919522110c60ff59741f5c96c02f734dc9
2243856-rgb <2243856@slu.edu.ph>
2026-05-13 01:26:33 +0800

    Define API_URL for consistent server URL usage

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
 
-const SERVER_URL = Platform.OS === 'android' ? https://nestvetapplication-e2a0bzagaka3bhfq.eastasia-01.azurewebsites.net/api/v1
+// Make sure to include the /api/v1 at the end!
+const API_URL = "https://nestvetapplication-e2a0bzagaka3bhfq.eastasia-01.azurewebsites.net/api/v1";
 
 const api = axios.create({
     baseURL: SERVER_URL,
