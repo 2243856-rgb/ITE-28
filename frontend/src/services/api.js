ba6026f213f3c4d5cfaad01ac310464ce3b0f797
2243856-rgb <2243856@slu.edu.ph>
2026-05-13 01:38:55 +0800

    Fix API_URL endpoint format
    Updated API_URL to use the correct endpoint format.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
 
-// Make sure to include the /api/v1 at the end!
 const API_URL = "https://nestvetapplication-e2a0bzagaka3bhfq.eastasia-01.azurewebsites.net/api/v1";
 
 const api = axios.create({
