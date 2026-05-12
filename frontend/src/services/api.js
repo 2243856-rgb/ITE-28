2c409ac174d5ee00c82e39a514707988c220a31d
2243856-rgb <2243856@slu.edu.ph>
2026-05-13 02:06:19 +0800

    Refactor API URL to use axios instance

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
 
-const API_URL = "https://nestvetapplication-e2a0bzagaka3bhfq.eastasia-01.azurewebsites.net/api/v1";
+const api = axios.create({
+  baseURL: 'https://nestvetapplication-e2a0bzagaka3bhfq.eastasia-01.azurewebsites.net/api/v1'
+});
 
 const api = axios.create({
     baseURL: SERVER_URL,
@@ -9,5 +11,3 @@ const api = axios.create({
         'Content-Type': 'application/json',
     },
 });
-
-export default api;
