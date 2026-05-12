47b7556d060dbba75af34911e2fb05b9f29202f7
2243856-rgb <2243856@slu.edu.ph>
2026-05-13 07:41:51 +0800

    Refactor global styles to use hardcoded colors
    Removed colors import and replaced with hardcoded hex codes for styling.

--- frontend/src/theme/globalStyles.js
+++ frontend/src/theme/globalStyles.js
@@ -1,62 +1,34 @@
 import { StyleSheet } from "react-native";
-import colors from "./colors/theme";
+
+// We removed the colors import because the file doesn't exist.
+// Hardcoded hex codes are used instead.
 
 export default StyleSheet.create({
     screen: {
         flex: 1,
-        backgroundColor: colors.background,
+        backgroundColor: "#FFFFFF",
     },
-
     container: {
         flex: 1,
-        paddingHorizontal: 22,
-        paddingTop: 20,
+        padding: 20,
     },
-
-    headerContainer: {
-        marginBottom: 26,
-    },
-
-    title: {
-        fontSize: 30,
-        fontWeight: "900",
-
-        color: colors.black,
-
-        letterSpacing: 2,
-    },
-
-    subtitle: {
-        marginTop: 6,
-
-        fontSize: 13,
-
-        color: colors.darkRed,
-
-        fontWeight: "800",
-
-        letterSpacing: 1,
-    },
-
-    section: {
-        backgroundColor: colors.white,
-
-        borderWidth: 3,
-        borderColor: colors.black,
-
-        borderRadius: 12,
-
-        padding: 18,
-
-        marginBottom: 18,
-    },
-
     topAccent: {
-        height: 14,
-
-        backgroundColor: colors.red,
-
-        borderBottomWidth: 3,
-        borderBottomColor: colors.black,
+        height: 5,
+        backgroundColor: "#007BFF",
+    },
+    headerContainer: {
+        marginBottom: 20,
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: "bold",
+        color: "#000000",
+    },
+    subtitle: {
+        fontSize: 18,
+        color: "#666666",
+    },
+    section: {
+        marginBottom: 20,
     },
 });
