bbc27d6029578633b4ff5f110c198bf65485a68e
2243856-rgb <2243856@slu.edu.ph>
2026-05-13 02:02:54 +0800

    Refactor API service to use constant for backend URL
    Removed hardcoded backend URL and used a constant instead.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
 
-// This is your official Azure Backend URL
-const AZURE_BACKEND_URL = 'https://nestvetapplication-e2a0bzagaka3bhfq.eastasia-01.azurewebsites.net/api/v1';
+const api = axios.create({
+  baseURL: 'https://nestvetapplication-e2a0bzagaka3bhfq.eastasia-01.azurewebsites.net/api/v1'
+});
 
 const api = axios.create({
     baseURL: AZURE_BACKEND_URL,
     headers: {
         'Content-Type': 'application/json',
     },
-    // This allows your frontend to send and receive cookies (important for Login/Auth)
     withCredentials: true 
 });
 
-// Optional: Add a simple interceptor to log errors for easier debugging in the browser console
 api.interceptors.response.use(
     (response) => response,
     (error) => {
